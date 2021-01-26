6513a88d7e61365c9ef296309472ab3e5e442ff0
Steven Johnstone <steven.james.johnstone@gmail.com>
2021-01-26 23:44:02 +0000

    Test comment extraction

--- testsuite/lua_test.go
+++ testsuite/lua_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"./parser"
-
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
@@ -40,6 +39,27 @@ func detectFunctions(program string) []fnCall {
 
 	antlr.ParseTreeWalkerDefault.Walk(tl, p.Chunk())
 	return tl.functionReceivers
+}
+
+func extractComments(program string) []string {
+	is := antlr.NewInputStream(program)
+
+	lexer := parser.NewLuaLexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, parser.LuaLexerCOMMENTS)
+
+	stream.Consume()
+
+	rv := []string{}
+
+	for _, token := range stream.GetAllTokens() {
+		switch token.GetTokenType() {
+		case parser.LuaParserCOMMENT:
+			fallthrough
+		case parser.LuaParserLINE_COMMENT:
+			rv = append(rv, token.GetText())
+		}
+	}
+	return rv
 }
 
 func TestDetectFunctions(t *testing.T) {
@@ -161,6 +181,60 @@ func TestDetectFunctions(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := detectFunctions(tt.args.program); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("DetectFunctions() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_extractComments(t *testing.T) {
+	type args struct {
+		program string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "basic: single-line comment",
+			args: args{
+				program: "--this is a comment",
+			},
+			want: []string{"--this is a comment"},
+		},
+		{
+			name: "basic: multiple single-line comments",
+			args: args{
+				program: "--this is a comment\n--this is also a comment",
+			},
+			want: []string{"--this is a comment\n", "--this is also a comment"},
+		},
+		{
+			name: "basic: comment after code",
+			args: args{
+				program: "fn(a) --this is a comment",
+			},
+			want: []string{"--this is a comment"},
+		},
+		{
+			name: "corner case: comment inside code",
+			args: args{
+				program: "fn(a --[[this is a comment]])",
+			},
+			want: []string{"--[[this is a comment]]"},
+		},
+		{
+			name: "basic: multiline comment",
+			args: args{
+				program: "--[[\nthis is a\ncomment\n]]",
+			},
+			want: []string{"--[[\nthis is a\ncomment\n]]"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := extractComments(tt.args.program); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("extractComments() = %+v, want %+v", got, tt.want)
 			}
 		})
 	}
